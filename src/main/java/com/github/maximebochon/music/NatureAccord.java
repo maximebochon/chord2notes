6a36ff8f24f22d9dc827705e063c4565b26da972
Maxime BOCHON <maxime.bochon@gmail.com>
2022-02-09 08:00:42 +0100

    chord nature, add boolean to know if the suffix should be superscript

--- src/main/java/com/github/maximebochon/music/NatureAccord.java
+++ src/main/java/com/github/maximebochon/music/NatureAccord.java
@@ -7,26 +7,39 @@ import static com.github.maximebochon.music.Intervalle.I;
 import static java.util.Arrays.asList;
 
 public enum NatureAccord {
-    MAJEUR("", asList(I(1), I(3), I(5))),
-    MAJEUR_SEPT("Δ", asList(I(1), I(3), I(5), I(7))),
-    SEPT("7", asList(I(1), I(3), I(5), I(7, BÉMOL))),
+    MAJEUR("", false,
+            asList(I(1), I(3), I(5))),
+    MAJEUR_SEPT("Δ", false,
+            asList(I(1), I(3), I(5), I(7))),
+    SEPT("7", false,
+            asList(I(1), I(3), I(5), I(7, BÉMOL))),
 
-    MINEUR("-", asList(I(1), I(3, BÉMOL), I(5))),
-    MINEUR_SEPT("-7", asList(I(1), I(3, BÉMOL), I(5), I(7, BÉMOL))),
+    MINEUR("−", false,
+            asList(I(1), I(3, BÉMOL), I(5))),
+    MINEUR_SEPT("−7", false,
+            asList(I(1), I(3, BÉMOL), I(5), I(7, BÉMOL))),
 
-    DIMINUÉ("O", asList(I(1), I(3, BÉMOL), I(5, BÉMOL))),
-    SEMI_DIMINUÉ("Ø", asList(I(1), I(3, BÉMOL), I(5, BÉMOL), I(7, BÉMOL)));
+    DIMINUÉ("O", true,
+            asList(I(1), I(3, BÉMOL), I(5, BÉMOL))),
+    SEMI_DIMINUÉ("Ø", true,
+            asList(I(1), I(3, BÉMOL), I(5, BÉMOL), I(7, BÉMOL)));
 
-    private String suffixe;
+    private final String suffixe;
+    private final boolean exposant;
     private final List<Intervalle> intervalles;
 
-    NatureAccord(final String suffixe, final List<Intervalle> intervalles) {
+    NatureAccord(final String suffixe, final boolean exposant, final List<Intervalle> intervalles) {
         this.suffixe = suffixe;
+        this.exposant = exposant;
         this.intervalles = intervalles;
     }
 
     public String getSuffixe() {
         return suffixe;
+    }
+
+    public boolean isExposant() {
+        return exposant;
     }
 
     public List<Intervalle> getIntervalles() {
